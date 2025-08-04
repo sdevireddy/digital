ee1cf35da8285ec58e480b64539e9064bee461ef
Siva <sendtosankar@gmail.com>
2025-08-04 22:30:08 +0530

    initial change

--- src/main/java/com/zen/digital/DigitalApplication.java
+++ src/main/java/com/zen/digital/DigitalApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.zen.digital.entity")
-@EnableJpaRepositories(basePackages = "com.zen.digital.repository")
+
 public class DigitalApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/zen/digital/filters/TenantConnectionProviderImpl.java
+++
@@ -1,5 +0,0 @@
-package com.zen.digital.filters;
-
-public class TenantConnectionProviderImpl {
-
-}
